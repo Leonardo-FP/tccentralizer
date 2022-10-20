66085e082aa20e088228c0b959179681e9dd47d2
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2022-10-19 21:11:47 -0300

    att

--- telas/sair.php
+++ telas/sair.php
@@ -1,15 +1,8 @@
 <?php
     session_start();
-    $token = md5(session_id());
-    if(isset($_GET['token']) && $_GET['token'] === $token) {
-        session_destroy();
-
-        echo "<script>location.href='../index.php';</script>";
-        
-        exit();
-
-    } else {
-        echo '<a class="btn btn-lg btn-primary btn-block" href="sair.php?token='.$token.'>Confirmar logout</a>';
-    }
+   
+    unset ($_SESSION['nome']);
+    unset ($_SESSION['senha']);
+    echo "<script>location.href='../index.php';</script>";
 
 ?>
