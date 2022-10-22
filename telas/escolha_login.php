e6e78e30feee13c256599e0867cef2a5bac88372
Kayke Matias Souza dos Santos <kayke.matias@outlook.com>
2022-10-22 16:19:45 -0300

    Correções e de estilização

--- telas/escolha_login.php
+++ telas/escolha_login.php
@@ -5,11 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
     <title>Selecionar o tipo de login</title>
 </head>
 <body>
-
-    <div>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/tccentralizer/index.php">TCCentralizer</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="escolha_login.php">Login</a></li>
+                <li><a href="escolha_cadastro.php">Cadastro</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="home">
         
         <h2>Selecione como deseja logar</h2>
 
