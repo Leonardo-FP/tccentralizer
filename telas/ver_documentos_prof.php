a9bfa2802998b5f74a75ea0cd792d71a53ff5d5d
Leonardo Franca Paulelli <90411076+Leonardo-FP@users.noreply.github.com>
2022-10-23 18:17:49 -0300

    adicionei botão de voltar

--- telas/ver_documentos_prof.php
+++ telas/ver_documentos_prof.php
@@ -40,6 +40,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div><a href="home.php" class="btn btn-primary">Voltar</a></div>
     <div style="padding: 10px;">
     <?php 
         if(!empty($infos_entrega)){ ?>
